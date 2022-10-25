b487680cda3f08815d219c210c3a4d781ab1419d
William Ekdahl <viljam89@hotmail.com>
2022-10-26 00:08:56 +0200

    inputfield

---
+++ src/components/AddUser.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {getUser} from '../secret/FetchUser.js';
+
+    let newUser;
+    let steamId;
+
+    async function addUser(){
+
+        getUser(steamId);
+
+    }
+
+</script>
+
+<div>
+    <label> SteamId
+        <input bind:value={steamId}>
+    </label>
+    <button on:click={() => addUser(steamId)}>Add user</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import AddUser from '../components/AddUser.svelte'
+
+</script>
+
+<h1>PGL</h1>
+<div>
+    <AddUser/>
+</div>
+
